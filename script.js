12b1101627ff18e1cdf1eecab635a2f85e86ca60
bhaktivedantdrs <67429356+bhaktivedantdrs@users.noreply.github.com>
2020-06-27 21:39:15 +0530

    executor side time tracker UI done.

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
     autosize($('textarea'));
 });
+
+
+$("#projectname").change(function() {
+    if(document.getElementById("projectname").value != ""){
+        $('#task').show();
+        if(document.getElementById("taskname").value != ""){
+            $('#activity').show();
+        }
+        else{
+            $('#activity').hide();
+        }
+    }
+    else{
+        $('#task').hide();
+        $('#activity').hide();
+
+    }
     
+});
+$("#taskname").change(function() {
+    console.log(document.getElementById("taskname").value);
+    if(document.getElementById("taskname").value != ""){
+        $('#activity').show();
+    }
+    else{
+        $('#activity').hide();
+    }
+});
+
